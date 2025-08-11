929080174777d970eb0c170169ae46c07dbecfe7
haneen <49983834+haneenhassen@users.noreply.github.com>
2025-08-11 11:07:46 +0300

    Update runs_qfb.sh

--- runs_qfb.sh
+++ runs_qfb.sh
@@ -6,15 +6,14 @@ for num in 41 42 43
 do
 MIN=0
 MAX=$num
-env="Gravitar-v4"
 
 # Generate a random integer
 seed=$(( MIN + RANDOM % (MAX - MIN + 1) ))
 echo "seed = $seed"
 
-python rlexplore_with_sb3_Atari.py --timesteps=$timesteps --seed=$seed  --batchsize=$batch --env=$env --model=$model &
-python rlexplore_with_sb3_Atari_E3B.py --timesteps=$timesteps --seed=$seed --batchsize=$batch --env=$env --model=$model &
-python rlexplore_with_sb3_Atari_NGU.py --timesteps=$timesteps --seed=$seed --batchsize=$batch --env=$env --model=$model &
-python rlexplore_with_sb3_Atari_RE3.py --timesteps=$timesteps --seed=$seed --batchsize=$batch --env=$env --model=$model &
+python rlexplore_with_sb3_Atari.py --timesteps=$timesteps --seed=$seed  --batchsize=$batch  &
+python rlexplore_with_sb3_Atari_E3B.py --timesteps=$timesteps --seed=$seed --batchsize=$batch  &
+python rlexplore_with_sb3_Atari_NGU.py --timesteps=$timesteps --seed=$seed --batchsize=$batch  &
+python rlexplore_with_sb3_Atari_RE3.py --timesteps=$timesteps --seed=$seed --batchsize=$batch &
 done
 
